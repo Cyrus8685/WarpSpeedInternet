c52173933b8116ab9ea47bdd6cff0fa0415ccf90
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-04-18 14:19:03 -0400

    auto search SDK 3

--- HomeScreen.js
+++ HomeScreen.js
@@ -28,66 +28,66 @@
            });
            
            const prepareRequest = async (lookupValue) => {
-           const SmartySDK = require("node_modules/smartystreets-javascript-sdk");
-           const SmartyCore = SmartySDK.core;
-           const Lookup = SmartySDK.usAutocompletePro.Lookup;
-           
-           // for Server-to-server requests, use this code:
-           // let authId = process.env.SMARTY_AUTH_ID;
-           // let authToken = process.env.SMARTY_AUTH_TOKEN;
-           // const credentials = new SmartyCore.StaticCredentials(authId, authToken);
-           
-           // for client-side requests (browser/mobile), use this code:
-           let key = "196979706711951117";
-           const credentials = new SmartyCore.SharedCredentials(key);
-           
-           // The appropriate license values to be used for your subscriptions
-           // can be found on the Subscription page of the account dashboard.
-           // https://www.smarty.com/docs/cloud/licensing
-           let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["us-autocomplete-pro-cloud"]);
-           // .withBaseUrl("");
-           let client = clientBuilder.buildUsAutocompleteProClient();
-           
-           // Documentation for input fields can be found at:
-           // https://www.smarty.com/docs/cloud/us-autocomplete-api#pro-http-request-input-fields
-           
-           // *** Simple Lookup ***
-           let lookup = new Lookup(lookupValue);
-           
-           await handleRequest(lookup, "Simple Lookup");
-           
-           // *** Using Filter and Prefer ***
-           /*lookup = new Lookup("4770 Lincoln");
-           
-           lookup.maxResults = 10;
-           lookup.includeOnlyCities = ["Chicago,La Grange,IL", "Blaine,WA"];
-           lookup.preferStates = ["IL"];
-           lookup.preferRatio = 33;
-           lookup.source = "all";
-           
-           await handleRequest(lookup, "Using Filter and Prefer");
-           
-           // *** Using 'selected' to Expand Secondaries ***
-           lookup = new Lookup("4770 Lincoln");
-           
-           lookup.selected = "4770 N Lincoln Ave Ste 2 (3) Chicago, IL 60625";
-           
-           await handleRequest(lookup, "Using 'selected' to Expand Secondaries") */
-           
-           // ************************************************
-           
-           function logSuggestions(response, message) {
-               console.log(message);
-               console.log(response.result);
-               console.log("*********************");
-           }
-           
-           async function handleRequest(lookup, lookupType) {
-               try {
-                   const results = await client.send(lookup);
-                   logSuggestions(results, lookupType);
-               } catch(err) {
-                   console.log(err)
-               }
-           }
+            const SmartySDK = require("smartystreets-javascript-sdk");
+            const SmartyCore = SmartySDK.core;
+            const Lookup = SmartySDK.usAutocompletePro.Lookup;
+            
+            // for Server-to-server requests, use this code:
+            // let authId = process.env.SMARTY_AUTH_ID;
+            // let authToken = process.env.SMARTY_AUTH_TOKEN;
+            // const credentials = new SmartyCore.StaticCredentials(authId, authToken);
+            
+            // for client-side requests (browser/mobile), use this code:
+            let key = "196979706711951117"
+            const credentials = new SmartyCore.SharedCredentials(key);
+            
+            // The appropriate license values to be used for your subscriptions
+            // can be found on the Subscription page of the account dashboard.
+            // https://www.smarty.com/docs/cloud/licensing
+            let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["us-autocomplete-pro-cloud"]);
+            // .withBaseUrl("");
+            let client = clientBuilder.buildUsAutocompleteProClient();
+            
+            // Documentation for input fields can be found at:
+            // https://www.smarty.com/docs/cloud/us-autocomplete-api#pro-http-request-input-fields
+            
+            // *** Simple Lookup ***
+            let lookup = new Lookup("Lookup");
+            
+            await handleRequest(lookup, "Simple Lookup");
+            
+            // *** Using Filter and Prefer ***
+           /* lookup = new Lookup("4770 Lincoln");
+            
+            lookup.maxResults = 10;
+            lookup.includeOnlyCities = ["Chicago,La Grange,IL", "Blaine,WA"];
+            lookup.preferStates = ["IL"];
+            lookup.preferRatio = 33;
+            lookup.source = "all";
+            
+            await handleRequest(lookup, "Using Filter and Prefer");
+            
+            // *** Using 'selected' to Expand Secondaries ***
+            lookup = new Lookup("4770 Lincoln");
+            
+            lookup.selected = "4770 N Lincoln Ave Ste 2 (3) Chicago, IL 60625";
+            
+            await handleRequest(lookup, "Using 'selected' to Expand Secondaries") */
+            
+            // ************************************************
+            
+            function logSuggestions(response, message) {
+                console.log(message);
+                console.log(response.result);
+                console.log("*********************");
+            }
+            
+            async function handleRequest(lookup, lookupType) {
+                try {
+                    const results = await client.send(lookup);
+                    logSuggestions(results, lookupType);
+                } catch(err) {
+                    console.log(err)
+                }
+            }
            };
