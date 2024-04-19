576ad137547c7e4cf6ee72d3a98065ab76a58e20
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-04-19 18:20:13 -0400

    auto search SDK 6

--- HomeScreen.js
+++ HomeScreen.js
@@ -20,74 +20,34 @@
             }
            }*/
 
-           const addressInput = document.querySelector('#address');
-           const suggestionsOutput = document.getElementById("result");
-           
-           addressInput.addEventListener("keyup" , (e) => {
-               prepareRequest(e.target.value);
-           });
-           
-           const prepareRequest = async (lookupValue) => {
-            const SmartySDK = require("smartystreets-javascript-sdk");
-            const SmartyCore = SmartySDK.core;
-            const Lookup = SmartySDK.usAutocompletePro.Lookup;
-            
-            // for Server-to-server requests, use this code:
-            // let authId = process.env.SMARTY_AUTH_ID;
-            // let authToken = process.env.SMARTY_AUTH_TOKEN;
-            // const credentials = new SmartyCore.StaticCredentials(authId, authToken);
-            
-            // for client-side requests (browser/mobile), use this code:
-            let key = "196979706711951117"
-            const credentials = new SmartyCore.SharedCredentials(key);
-            
-            // The appropriate license values to be used for your subscriptions
-            // can be found on the Subscription page of the account dashboard.
-            // https://www.smarty.com/docs/cloud/licensing
-            let clientBuilder = new SmartyCore.ClientBuilder(credentials).withLicenses(["us-autocomplete-pro-cloud"]);
-            // .withBaseUrl("");
-            let client = clientBuilder.buildUsAutocompleteProClient();
-            
-            // Documentation for input fields can be found at:
-            // https://www.smarty.com/docs/cloud/us-autocomplete-api#pro-http-request-input-fields
-            
-            // *** Simple Lookup ***
-            let lookup = new Lookup("Lookup");
-            
-            await handleRequest(lookup, "Simple Lookup");
-            
-            // *** Using Filter and Prefer ***
-           /* lookup = new Lookup("4770 Lincoln");
-            
-            lookup.maxResults = 10;
-            lookup.includeOnlyCities = ["Chicago,La Grange,IL", "Blaine,WA"];
-            lookup.preferStates = ["IL"];
-            lookup.preferRatio = 33;
-            lookup.source = "all";
-            
-            await handleRequest(lookup, "Using Filter and Prefer");
-            
-            // *** Using 'selected' to Expand Secondaries ***
-            lookup = new Lookup("4770 Lincoln");
-            
-            lookup.selected = "4770 N Lincoln Ave Ste 2 (3) Chicago, IL 60625";
-            
-            await handleRequest(lookup, "Using 'selected' to Expand Secondaries") */
-            
-            // ************************************************
-            
-            function logSuggestions(response, message) {
-                console.log(message);
-                console.log(response.result);
-                console.log("*********************");
-            }
-            
-            async function handleRequest(lookup, lookupType) {
-                try {
-                    const results = await client.send(lookup);
-                    logSuggestions(results, lookupType);
-                } catch(err) {
-                    console.log(err)
-                }
-            }
-           };
+let autocomplete;
+async function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            componentRestrictions: {'country': ['US']},
+            fields: ['place_id', 'geometry', 'name','address_components',]
+        });
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+async function onPlaceChanged(){
+    var place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+        document.getElementById('address').placeholder = 'Enter a place';
+    } else {
+        document.getElementById('details').innerHTML = place.name;
+    }
+}
+
+addresscheck.addEventListener("click", async function  checkAvailability() {
+    for (let i= 0; i<result.length; i++) {
+        if (result[i].short_name === 'FL' ) {
+            alert("Within Florid");
+        }
+        else {
+            alert("Not Within Florida")
+        }
+    }
+});
