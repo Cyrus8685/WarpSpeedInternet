fb81dc2c7d511271992fe56dd4585a5dd8aad911
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-04-24 17:11:27 -0400

    Updating files 29

--- assets/js/lt.js
+++ assets/js/lt.js
@@ -1,8 +1,8 @@
 function changePage() {
-    window.location.assign("./html/plansPage.html");
+    window.location.assign("./assets/html/plansPage.html");
 };
     function changePage2() {
         var Lt = "Lt Plan $69.99 1000/1000 Mbps";
         localStorage.setItem('Lt', Lt);
-        window.location.assign("./html/cart.html");
+        window.location.assign("./assets/html/cart.html");
     };
