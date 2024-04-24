8a6a889c6f0fcf820feb9ee0af9eb128b06c6efa
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-04-24 11:41:48 -0400

    Updating files 12

--- assets/js/commander.js
+++ assets/js/commander.js
@@ -1,7 +1,7 @@
 async function changePage() {
-    window.location.assign("/assets/html/planspage.html");};
+    window.location.assign("http://assets/html/planspage.html");};
 
     async function changePage2() {
         var Commander = "Commander Plan $99.99 2000/2000 Mbps";
         localStorage.setItem('Commander', Commander.value);
-        window.location.assign("/assets/html/cart.html");};
+        window.location.assign("http://assets/html/cart.html");};
--- assets/js/ensign.js
+++ assets/js/ensign.js
@@ -1,11 +1,11 @@
 function changePage() {
-    window.location.assign("/assets/html/planspage.html");
+    window.location.assign("http://assets/html/planspage.html");
 };
 
     function changePage2() {
         var Ensign = "Ensign Plan $39.99 500/500 Mbps";
         localStorage.setItem('Ensign', Ensign.value);
-        window.location.assign("/assets/html/cart.html");
+        window.location.assign("http://assets/html/cart.html");
     };
 
     
