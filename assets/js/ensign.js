767f3d9a0719ba8f6462765e530e9b9f44333fea
Cyrus8685 <157924150+Cyrus8685@users.noreply.github.com>
2024-04-24 09:24:54 -0400

    Update ensign.js

--- assets/js/ensign.js
+++ assets/js/ensign.js
@@ -5,7 +5,7 @@ function changePage() {
     function changePage2() {
         var Ensign = "Ensign Plan $39.99 500/500 Mbps";
         localStorage.setItem('Ensign', Ensign.value);
-        window.location.assign("/assets/html/cart.html");
+        window.location.assign("./assets/html/cart.html");
     };
 
     
