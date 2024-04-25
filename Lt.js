76b958bb24eefe4ee755ec0bd61010ee4f51489a
Cyrus8685 <157924150+Cyrus8685@users.noreply.github.com>
2024-04-24 23:15:58 -0400

    Delete Lt.js

--- Lt.js
+++
@@ -1,8 +0,0 @@
-function changePage() {
-    window.location.replace("planspage.html")};
-
-    function changePage2() {
-        var Lt = "Lt";
-        localStorage.setItem('Lt', Lt);
-        window.location.replace("cart.html");
-    };
