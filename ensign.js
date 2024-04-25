5fdb85fc8754946970469b2fa6427fca74c4cc5f
Cyrus8685 <157924150+Cyrus8685@users.noreply.github.com>
2024-04-24 23:16:34 -0400

    Delete ensign.js

--- ensign.js
+++
@@ -1,9 +0,0 @@
-function changePage() {
-    window.location.replace("planspage.html")};
-
-    function changePage2() {
-        var Ensign = "Ensign";
-        localStorage.setItem('Ensign', Ensign.value);
-        window.location.replace("cart.html")};
-
-    
