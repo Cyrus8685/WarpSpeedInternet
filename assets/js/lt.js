ef0888a1f58a126ed3a4c4a0c5a2d7e2d5569f05
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-04-24 21:39:30 -0400

    Updating files 32

--- assets/js/lt.js
+++ assets/js/lt.js
@@ -1,8 +1,8 @@
 function changePage() {
-    window.location.assign("./assets/html/plansPage.html");
+    window.location.assign("/warpspeedinternet/assets/html/plansPage.html");
 };
     function changePage2() {
         var Lt = "Lt Plan $69.99 1000/1000 Mbps";
         localStorage.setItem('Lt', Lt);
-        window.location.assign("./assets/html/cart.html");
+        window.location.assign("/warpspeedinternet/assets/html/cart.html");
     };
--- assets/js/planspage.js
+++ assets/js/planspage.js
@@ -1,15 +1,15 @@
 function changePage2() {
-    window.location.assign("./assets/html/commander.html");
+    window.location.assign("/warpspeedinternet/assets/html/commander.html");
 };
 
 function changePage1() {
-    window.location.assign("./assets/html/ensign.html");
+    window.location.assign("/warpspeedinternet/assets/html/ensign.html");
 };
 
 function changePage3() {
-    window.location.assign("./assets/html/lt.html");
+    window.location.assign("/warpspeedinternet/assets/html/lt.html");
 };
 
 function changePage4() {
-    window.location.assign("./index.html");
+    window.location.assign("/warpspeedinternet/index.html");
 };
